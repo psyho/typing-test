01e5267e735225edf14970fd88b781bb2d617bdb
Adam Pohorecki <adam.pohorecki@llp.pl>
2012-03-27 16:35:02 +0200

    added js-test-driver-rails

---
+++ config/js_test_driver.rb
@@ -0,0 +1,8 @@
+browser 'google-chrome'
+
+includes 'app/assets/javascripts/vendor/**/*.js'
+includes 'app/assets/javascripts/app/**/*.js'
+
+enable_jasmine
+
+includes 'spec/javascripts/unit/**/*.js'
---
+++ spec/javascripts/unit/hello_world_spec.js
@@ -0,0 +1,5 @@
+describe('Hello', function() {
+  it('should do something', function() {
+    expect(2+2).toEqual(4);
+  });
+});
